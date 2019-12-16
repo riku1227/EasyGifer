3256548cd47a23d95148a60eea90f42d0583402d
riku1227 <17927874+riku1227@users.noreply.github.com>
2019-12-16 21:32:56 +0900

    🎉 Happy initial commit

---
+++ EasyGifer/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyGifer
+{
+    class MainWindowViewModel
+    {
+        public String InpuPath { get; set; } = "";
+        public String OutputPath { get; set; } = "";
+        public String GifWidth { get; set; } = "640";
+        public String GifFps { get; set; } = "30";
+    }
+}
